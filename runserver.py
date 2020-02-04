ec833693fda224c87bb5cb82ccecb642f0498591
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-02-04 23:28:54 +0000

    use PORT instead of FLASK_RUN_PORT so it works on Heroku

--- runserver.py
+++ runserver.py
@@ -6,6 +6,6 @@ import os
 if __name__ == '__main__':
     if os.getenv('ENV', '') == 'test':
         rebuild()
-    port = int(os.getenv("FLASK_RUN_PORT", "80"))
+    port = int(os.getenv("PORT", "80"))
     debug = os.getenv("FLASK_DEBUG") == "1"
     app.run(host='0.0.0.0', port=port, debug=debug)
