93d1a9459a6bdaa3e13a2824bca61764a87cfd39
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-02-09 17:50:34 +0000

    auto-reply to received slack messages

--- cog/tools/slack.py
+++ cog/tools/slack.py
@@ -28,3 +28,21 @@ def send_slack(email, message):
     if not response["ok"]:
         return False
     return True
+
+@slack.RTMClient.run_on(event='message')
+def recv_slack_message(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    rtm_client = payload['rtm_client']
+    # if 'Hello' in data.get('text', []):
+    channel_id = data['channel']
+    thread_ts = data['ts']
+    user = data['user']
+    web_client.chat_postMessage(
+        channel=channel_id,
+        text=f"Hi <@{user}>! This is an unmonitored Slack bot. Please direct all messages or questions to one of the organizers, or email hello@treehacks.com.",
+        thread_ts=thread_ts
+    )
+
+rtm_client = slack.RTMClient(token=SLACK_OAUTH_ACCESS_TOKEN)
+rtm_client.start()
